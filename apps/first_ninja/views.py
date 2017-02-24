c74603d4fe3429ca63a20b62eea8c5082245fd1d
Aly Hemani <alyhemani@gmail.com>
2017-02-23 19:07:34 -0600

    update

--- apps/first_ninja/views.py
+++ apps/first_ninja/views.py
@@ -14,23 +14,23 @@ def process(request):
 	if request.method == "POST":
 		if 'farm' in request.POST:
 			number= random.randrange(10,21)
-			request.session['gold']+= number
-			request.session['log'] += 'Earned ' +str(number)+ ' gold from the farm!'
+			request.session['gold']= number
+			request.session['log'] = 'Earned ' +str(number)+ ' gold from the farm!'
 		elif 'cave' in request.POST:
 			number= random.randrange(5,11)
-			request.session['gold']+= number
-			request.session['log'] += 'Earned ' +str(number)+ ' gold from the cave!'
+			request.session['gold']= number
+			request.session['log'] = 'Earned ' +str(number)+ ' gold from the cave!'
 		elif 'house' in request.POST:
 			number= random.randrange(2,6)
-			request.session['gold']+= number
-			request.session['log'] += 'Earned ' +str(number)+ ' gold from the house!'
+			request.session['gold']= number
+			request.session['log'] = 'Earned ' +str(number)+ ' gold from the house!'
 		elif 'casino' in request.POST:
 			number= random.randrange(-50,50)
 			request.session['gold']+= number
 			if number > 0:
-				request.session['log'] += 'Earned ' +str(number)+ ' gold from the casino!'
+				request.session['log'] = 'Earned ' +str(number)+ ' gold from the casino!'
 			elif number == 0:
-				request.session['log'] += 'Earned 0 gold from the casino!'
+				request.session['log'] = 'Earned 0 gold from the casino!'
 			elif number < 0:
-				request.session['log'] += 'OUCH!!! Lost ' +str(abs(number))+ ' gold from the casino!'
+				request.session['log'] = 'OUCH!!! Lost ' +str(abs(number))+ ' gold from the casino!'
 		return redirect('/')
